3d2da4db229024968f33e00c53dd4be098221bf2
Nolan <nolan.m.mcallister@gmail.com>
2021-03-13 17:22:19 -0700

    milestone 2 update

---
+++ analysis/nolan/NolanMarkdown.md
@@ -0,0 +1,16 @@
+# Nolan
+---
+**This file is just for Task 1 P2 of Milestone 2!**
+
+There is *several* things you can do with markdowns
+
+**lists**
+1. 1st item
+2. 2nd item
+3. 3rd item
+4. 4th item
+* WOW
+- A
++ List!
+
+Cool mark down is *seriously* cool!
--- analysis/scripts/project_functions.py
+++ analysis/scripts/project_functions.py
@@ -1,11 +1,15 @@
+import pandas as pd
+
 def load_and_process(url_or_path_to_csv_file):
 
     # Method Chain 1 (Load data and deal with missing data)
 
     df1 = (
           pd.read_csv(url_or_path_to_csv_file)
-          .rename(...)
-          .dropna(...)
+          .rename(columns={'age': 'Age', 'sex': 'Sex', 'bmi': 'BMI',
+                           'children': 'Children', 'smoker': 'Smoker', 'region': 'Region',
+                           'charges': 'Charges'})
+          .dropna()
           # etc...
       )
 
@@ -13,11 +17,54 @@ def load_and_process(url_or_path_to_csv_file):
 
     df2 = (
           df1
-          .assign(...)
+          .assign(Body_Shape=get_body_shapes(df1))
       )
 
     # Make sure to return the latest dataframe
 
-    return df2 
+    return df2
+
+def get_body_shapes(df):
+    body_shapes = []
+    
+    for bmi in df['BMI']:
+        if bmi < 18:
+            body_shapes.append('Under Weight')
+            
+        elif 18 <= bmi < 23:
+            body_shapes.append('Normal Weight')
+            
+        elif 23 <= bmi < 27:
+            body_shapes.append('Over Weight')
+            
+        elif 27 <= bmi < 32:
+            body_shapes.append('Class 1 Obesity')
+            
+        elif 32 <= bmi < 37:
+            body_shapes.append('Class 2 Obesity')
+            
+        elif 37 <= bmi:
+            body_shapes.append('Class 3 Obesity')
+
+    return body_shapes
+
+def compare_smokers_to_bmi(df):
+    data = df[['Smoker', 'Body_Shape']]
+    
+    new_data = pd.DataFrame([[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]],
+                        index=['Under Weight', 'Normal Weight', 'Over Weight', 'Class 1 Obesity', 'Class 2 Obesity', 'Class 3 Obesity'],
+                        columns=['Population', 'Non-Smokers', 'Smokers'])
+
+
+    for i in range(len(data)):
+        new_data['Population'][data.loc[i]['Body_Shape']] += 1
+        
+        if data.loc[i]['Smoker'] == 'no':
+            new_data['Non-Smokers'][data.loc[i]['Body_Shape']] += 1
+    
+        else:
+            new_data['Smokers'][data.loc[i]['Body_Shape']] += 1
+
+    return new_data
 
 #### This is currently just a copy / paste from the course content, will add to it.
